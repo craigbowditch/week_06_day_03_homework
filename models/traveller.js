65d750c453a29eb932e96b70808533891d38e001
Craig Bowditch <craigbowditch@hotmail.com>
2018-08-23 10:04:46 +0100

    alternative get unique modes of transport method

--- models/traveller.js
+++ models/traveller.js
@@ -4,43 +4,50 @@ const Traveller = function(journeys) {
 
 Traveller.prototype.getJourneyStartLocations = function() {
   return this.journeys.map((journey) => {
-    return journey.startLocation
+    return journey.startLocation;
   })
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
   return this.journeys.map((journey) => {
-    return journey.endLocation
+    return journey.endLocation;
   })
 };
 
 Traveller.prototype.getModesOfTransport = function () {
   return this.journeys.map((journey) => {
-    return journey.transport
+    return journey.transport;
   })
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
   return this.journeys.filter((journey) => {
-    return journey.transport === transport
+    return journey.transport === transport;
   })
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
   return this.journeys.filter((journey) => {
-    return journey.distance > minDistance
+    return journey.distance > minDistance;
   })
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
   return this.journeys.reduce((acc, curr) => {
-    return acc + curr.distance
+    return acc + curr.distance;
   }, 0)
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-  return [...new Set(this.getModesOfTransport())]
+  return [...new Set(this.getModesOfTransport())];
 };
+
+// Traveller.prototype.getUniqueModesOfTransport = function () {
+//   let transportation = this.getModesOfTransport();
+//   return transportation.filter((mode, index) => {
+//     return transportation.indexOf(mode) === index;
+// });
+// Alternative way of doing it using index
 
 
 module.exports = Traveller;
